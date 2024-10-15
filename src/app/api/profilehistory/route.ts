1071953a60d59e189fe76f073e21247cb961b600
venkateshramkumarg <venkateshramkumar3@gmail.com>
2024-10-16 00:02:39 +0530

    Refactor route.ts to remove unnecessary line

--- src/app/api/profilehistory/route.ts
+++ src/app/api/profilehistory/route.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import prisma from "@/utils/prisma";
 
 
+
 export async function POST(req:NextRequest) {
     try {
         const body = await req.json();
